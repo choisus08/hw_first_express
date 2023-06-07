a4a50d5dd21716214bdf79d098ccea94cdcf74ae
Susie Gordon <susieyoungchoi@gmail.com>
2023-06-07 09:47:48 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`<h1>${name}! It's so great to see you!`)
 });
 
+
 // Tip Calculator
 
 app.get("/tip/:total/:tipPercentage", (req, res) => {
@@ -23,6 +24,21 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     } else {
         res.send("Invalid parameters")
     }  
+});
+
+
+// Magic 8 Ball
+
+app.get("/magic/:question", (req, res) => {
+    // req string input
+    const question = req.params.question;
+    const magic8 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+    // return random response from magic8
+    const magicRandom = Math.floor(Math.random() * magic8.length);
+
+    res.send(`${question}? <h1>${magic8[magicRandom]}</h1>`)
+
 });
 
 
